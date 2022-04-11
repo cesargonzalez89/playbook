31be41aa819bf8d07f0953b1775d884ec40e2e84
Cesar Gonzalez <jugador409@hotmail.com>
2022-04-10 21:29:05 -0500

    Carga de los 10 primeros ejercicios de js

---
+++ weekly_mission_1/example10/pokemon.js
@@ -0,0 +1,15 @@
+export default class pokemon {
+
+    constructor (name) {
+        this.name = name
+      }
+    
+      sayHello (message) {
+        console.log(`Mi pokemon ${this.name} te saluda!!!`)
+      }
+  
+      sayMessage (message) {
+          console.log(`Mi pokemon ${this.name} dice: ${message}`)
+      }
+      
+  }
